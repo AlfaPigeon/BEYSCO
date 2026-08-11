a08abd4043534019362b2031f1eb31c43f8598e2
Oğuz Kaan Özsoy <51211384+AlfaPigeon@users.noreply.github.com>
2026-08-11 15:57:26 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  base: '/BEYSCO',
+  base: '',
   outDir: 'docs',
   integrations: [tailwind()],
   i18n: {
